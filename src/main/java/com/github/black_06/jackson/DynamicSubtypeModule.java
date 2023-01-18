3fc12f01e2efac9253d329dfd15e2cd93b9220e8
black <hello.bug@foxmail.com>
2023-01-18 12:57:46 +0800

    docs: add docs in module

--- src/main/java/com/github/black_06/jackson/DynamicSubtypeModule.java
+++ src/main/java/com/github/black_06/jackson/DynamicSubtypeModule.java
@@ -9,13 +9,34 @@ import com.google.auto.service.AutoService;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
+/**
+ * Dynamic subtype module.
+ * <p>
+ * The module caches the subclass, so it's non-real-time.
+ * It's for registering subtypes without annotating the parent class.
+ * See <a href="https://github.com/FasterXML/jackson-databind/issues/2104">this issues<a/> in jackson-databind.
+ * <p>
+ * When not found in the cache, it loads and caches subclasses using SPI.
+ * Therefore, we can {@link #unregisterType} a class and then module will reload this class's subclasses.
+ */
 @AutoService(Module.class)
 public class DynamicSubtypeModule extends Module {
-    private final HashMap<Class<?>, List<NamedType>> subtypes = new HashMap<>();
+
+    /**
+     * if true, module uses {@link java.util.ServiceLoader}.
+     * if false, module uses {@link ServiceLoader} modified from Oracle JDK 1.8.0_291-b10
+     * <p>
+     * It's tested in Oracle JDK 1.8.0_291-b10 and Open JDK 17+35-LTS-2724
+     * <p>
+     * When {@link ServiceLoader} doesn't work in other version, open this setting.
+     * NOTE: {@link java.util.ServiceLoader} must need a no args constructor in subclass.
+     */
+    private boolean useStandardServiceLoader;
+    private final ConcurrentHashMap<Class<?>, List<NamedType>> subtypes = new ConcurrentHashMap<>();
 
     @Override
     public String getModuleName() {
@@ -52,18 +73,48 @@ public class DynamicSubtypeModule extends Module {
         });
     }
 
-    private void registerTypes(Class<?> parent) {
+    public void setUseStandardServiceLoader(boolean useStandardServiceLoader) {
+        this.useStandardServiceLoader = useStandardServiceLoader;
+    }
+
+    /**
+     * load parent's subclass by SPI.
+     *
+     * @param parent parent class.
+     */
+    @SuppressWarnings("unchecked")
+    public <S> void registerTypes(Class<S> parent) {
         if (subtypes.containsKey(parent)) {
             return;
         }
+        if (useStandardServiceLoader) {
+            List<Class<S>> subclasses = new ArrayList<>();
+            for (S instance : java.util.ServiceLoader.load(parent)) {
+                subclasses.add((Class<S>) instance.getClass());
+            }
+            this.registerTypes(parent, subclasses);
+        } else {
+            this.registerTypes(parent, ServiceLoader.load(parent));
+        }
+    }
+
+    /**
+     * register subtypes without SPI.
+     * Of course, you need to provide them :)
+     */
+    public <S> void registerTypes(Class<S> parent, Iterable<Class<S>> subclasses) {
         List<NamedType> result = new ArrayList<>();
-        for (Class<?> subclass : ServiceLoader.load(parent)) {
+        for (Class<S> subclass : subclasses) {
             result.addAll(findSubtypes(subclass, subclass::getAnnotation));
         }
         subtypes.put(parent, result);
     }
 
-    private static List<NamedType> findSubtypes(Class<?> clazz, Function<Class<JsonSubType>, JsonSubType> getter) {
+    public void unregisterType(Class<?> parent) {
+        subtypes.remove(parent);
+    }
+
+    private static <S> List<NamedType> findSubtypes(Class<S> clazz, Function<Class<JsonSubType>, JsonSubType> getter) {
         if (clazz == null) {
             return Collections.emptyList();
         }
